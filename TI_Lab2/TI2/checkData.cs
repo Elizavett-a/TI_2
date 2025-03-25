a85370d1a7964b2f98d206ac0fbb1ee37b8e4904
Лизик <145809336+Elizavett-a@users.noreply.github.com>
2025-03-25 10:26:17 +0300

    update project

---
+++ TI_Lab2/TI2/checkData.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace TI_Lab2
+{
+    public class checkData
+    {
+        public static string CheckForBD(string str)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char symbol in str)
+            {
+                if (symbol == '0' || symbol == '1')
+                {
+                    result.Append(symbol);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
